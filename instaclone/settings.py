fbeb26261c0a7659c8492f27d64fb1a794ff33c6
Bryson69 <brysonmundia@gmail.com>
2020-08-09 10:44:55 +0300

    Add Media for images

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'insta',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'insta',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = 'media'
+
+MEDIA_ROOT = os.path.join(BASE_DIR.'media')
