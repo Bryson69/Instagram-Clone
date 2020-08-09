87a8a3999ed1516ee175b0409ba0cee7f2662581
Bryson69 <brysonmundia@gmail.com>
2020-08-09 22:15:34 +0300

    Add url routing for insta/urls

--- insta/urls.py
+++ insta/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import (
-    PostListView
+    PostListView,
+    PostCreateView,
 )
 
 app_name = 'insta'
@@ -8,4 +9,5 @@ app_name = 'insta'
 urlpatterns = [
     # Local : http://127.0.0.1:8000/admin
     path('', PostListView.as_view(), name='post_list'),
+    path('new/', PostCreateView.as_view(), name = 'post_create'),
 ]
