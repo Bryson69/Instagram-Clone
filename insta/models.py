112bce9b80e063e0e8ef69b8c6143a5e4e0e806a
Bryson69 <brysonmundia@gmail.com>
2020-08-19 07:14:25 +0300

    Deployment

--- insta/models.py
+++ insta/models.py
@@ -5,6 +5,27 @@ from friendship.models import Friend,Follow,Block
 
 # Create your models here.
 
+class Image(models.Model):
+    pic=ImageField(manual_crop='1080x800', blank=True)
+    name= models.CharField(max_length=55)
+    caption = models.TextField(blank=True)
+    profile= models.ForeignKey(User, blank=True,on_delete=models.CASCADE)
+
+
+
+    def __str__(self):
+        return str(self.name)
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def get_profile_images(cls, profile):
+        images = Image.objects.filter(profile__pk=profile)
+        return images
 
 
 class Profile(models.Model):
@@ -36,27 +57,6 @@ class Profile(models.Model):
 
 
 
-class Image(models.Model):
-    pic=ImageField(manual_crop='1080x800', blank=True)
-    name= models.CharField(max_length=55)
-    caption = models.TextField(blank=True)
-    profile= models.ForeignKey(User, blank=True,on_delete=models.CASCADE)
-
-
-
-    def __str__(self):
-        return str(self.name)
-
-    def save_image(self):
-        self.save()
-
-    def delete_image(self):
-        self.delete()
-
-    @classmethod
-    def get_profile_images(cls, profile):
-        images = Image.objects.filter(profile__pk=profile)
-        return images
 
 class Comment(models.Model):
     image = models.ForeignKey(Image,blank=True, on_delete=models.CASCADE,related_name='comment')
